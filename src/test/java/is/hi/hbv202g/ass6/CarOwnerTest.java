c383bdc96c5e4a15f1c7ee32a2ec8718a69c345e
dbg14hi <dbg14@hi.is>
2023-02-21 10:49:45 +0000

    finito

--- src/test/java/is/hi/hbv202g/ass6/CarOwnerTest.java
+++ src/test/java/is/hi/hbv202g/ass6/CarOwnerTest.java
@@ -1,14 +1,23 @@
 package is.hi.hbv202g.ass6;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class CarOwnerTest {
 
+    public static final String BEETLE = "Beetle";
+    private Car beetle;
+
+    @Before
+    public void setUp() throws Exception {
+        beetle = new Car(BEETLE);
+    }
+
     @Test
     public void getName() {
-        Car car=new Car("Beetle");
+        Car car = beetle;
         CarOwner carOwner = new CarOwner("John Doe", car);
         assertEquals("John Doe", carOwner.getName());
     }
